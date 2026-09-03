7f9d32f6110aef3fd92d1cf3bbcdcf80d70975f6
Thamidu Chamod <thamiduchamod748@gmail.com>
2026-09-03 21:13:08 +0530

    feat: update pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,22 +1,60 @@
 module.exports = {
-  apps : [
-      {
-          name   : "user-service",
-          script : "java -jar ./user-service/target/user-service-0.0.1-SNAPSHOT.jar",
-          log_file: "./logs/user-service.log",
-          instances: 2
-      },
-      {
-          name   : "book-service",
-          script : "java -jar ./book-service/target/book-service-0.0.1-SNAPSHOT.jar",
-          log_file: "./logs/book-service.log",
-          instances: 2
-      },
-      {
-          name   : "borrow-service",
-          script : "java -jar ./borrow-service/target/borrow-service-0.0.1-SNAPSHOT.jar",
-          log_file: "./logs/borrow-service.log",
-          instances: 2
-      }
-  ]
-}
+    apps: [
+        {
+            name: "user-service",
+            script: "java",
+            args: "-jar ./user-service/target/user-service-0.0.1-SNAPSHOT.jar",
+            instances: 1,
+            env: {
+                SERVER_PORT: 8081
+            }
+        },
+        {
+            name: "user-service-2",
+            script: "java",
+            args: "-jar ./user-service/target/user-service-0.0.1-SNAPSHOT.jar",
+            instances: 1,
+            env: {
+                SERVER_PORT: 8082
+            }
+        },
+
+        {
+            name: "book-service",
+            script: "java",
+            args: "-jar ./book-service/target/book-service-0.0.1-SNAPSHOT.jar",
+            instances: 1,
+            env: {
+                SERVER_PORT: 8083
+            }
+        },
+        {
+            name: "book-service-2",
+            script: "java",
+            args: "-jar ./book-service/target/book-service-0.0.1-SNAPSHOT.jar",
+            instances: 1,
+            env: {
+                SERVER_PORT: 8084
+            }
+        },
+
+        {
+            name: "borrow-service",
+            script: "java",
+            args: "-jar ./borrow-service/target/borrow-service-0.0.1-SNAPSHOT.jar",
+            instances: 1,
+            env: {
+                SERVER_PORT: 8085
+            }
+        },
+        {
+            name: "borrow-service-2",
+            script: "java",
+            args: "-jar ./borrow-service/target/borrow-service-0.0.1-SNAPSHOT.jar",
+            instances: 1,
+            env: {
+                SERVER_PORT: 8086
+            }
+        }
+    ]
+};
