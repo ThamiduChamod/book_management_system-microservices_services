2b637c05045aad23282230107d0c2b07dddfb7eb
Thamidu Chamod <thamiduchamod748@gmail.com>
2026-09-04 20:54:52 +0530

    feat: update pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,6 +1,11 @@
 module.exports = {
     apps: [
         {
+            name: "cloud-sql-auth-proxy",
+            script: "./cloud-sql-proxy thermal-petal-506905-g0:asia-southeast1:postgre-vm --private-ip",
+            log_file: ".logs/cloud-sqlproxy.log",
+        },
+        {
             name: "user-service",
             script: "java",
             args: "-jar ./user-service/target/user-service-0.0.1-SNAPSHOT.jar",
